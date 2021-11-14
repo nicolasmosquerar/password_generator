ec1fcf1e2e7e29f660da9e0a4c7729adcf2d0b21
Nicolas <nicolas.mosquerar@hotmail.com>
2021-11-13 21:57:47 -0500

    se agrega la funcion principal  de password en la cual se crea la contraseña con mayusculas y minusculas tambien se trabaja con metodos get para estos

--- generator/views.py
+++ generator/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import random
 
 def about(request):
-    
-    return render(request, 'about.html')                                          
-
+    return render(request, 'about.html')
 
 
 def home(request):
+    return render(request, 'home.html')
+
+
+
+def password(request):
     
-    return render(request,'home.html')
+    characters = list('abcdefghijklmnñopqrstuwxyz')
+    generated_password=''
+    length=int(request.GET.get('length')) 
+    
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+        
+        
+    
+    for i in range(length):
+        
+        generated_password+=random.choice(characters)
+        
+    
+    return render(request, 'password.html',{'password':generated_password})
